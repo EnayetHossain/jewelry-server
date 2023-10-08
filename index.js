3924a2171201e7cbcc7ba81fedd330d7f947711d
EnayetHossain <enayethossasin135@gmail.com>
2023-10-08 09:25:12 +0600

    all get API

---
+++ index.js
@@ -0,0 +1,81 @@
+require("dotenv").config();
+const express = require('express');
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000
+
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// mongodb connection
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vahgs6d.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const jewelryCollection = client.db("jewelry").collection("jewels");
+
+    // get all jewelry data
+    app.get("/jewelry", async (req, res)=>{
+        const query = {};
+        const result = await jewelryCollection.find(query).toArray();
+        res.send(result);
+    });
+
+    //get single jewelry data
+    app.get("/jewelry/:id", async (req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await jewelryCollection.findOne(query);
+        res.send(result);
+    });
+
+    // get all jewelry of a single user
+    app.get("/my-jewelry", async (req, res)=>{
+        let query = {};
+
+        if(req.query?.email){
+            query = {email: req.query.email}
+        }else{
+            res.status(403).send({message: "Bad request"})
+        }
+
+        const result = await jewelryCollection.find(query).toArray();
+        res.send(result);
+    });
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// root route
+app.get("/", (req, res)=>{
+    res.send("server running");
+});
+
+app.listen(port, ()=>{
+    console.log(`visit http://localhost:${port}`);
+});
